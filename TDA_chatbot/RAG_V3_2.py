f7b0ca0d8ed4c6ff8f89e9b03863cd161bb2476a
LOO QI CHYE <134897004+22038809-QICHYE@users.noreply.github.com>
2025-02-06 13:33:27 +0800

    Update RAG_V3_2.py
    added:
    process_query_v3 function along with its test function which uses Multi query with BM25 reranking

--- TDA_chatbot/RAG_V3_2.py
+++ TDA_chatbot/RAG_V3_2.py
@@ -1,7 +1,7 @@
 from Augment import Augmentation
 from Retrieval import Retriever
-from Re_ranker import CrossEncoderReRanker
-from Memory import ExactMatchRedisCache
+from Re_ranker import CrossEncoderReRanker, BM25ReRanker
+from Memory import RedisSemanticCacheManager, ExactMatchRedisCache
 
 class RAGSystem:
     def __init__(self):
@@ -10,10 +10,12 @@ class RAGSystem:
         """
         self.retriever = Retriever()
         self.re_ranker = CrossEncoderReRanker()
+        self.re_ranker2 = BM25ReRanker()
         self.augmenter = Augmentation()
-        self.cache_manager = ExactMatchRedisCache() # exact match dont need llm_string
+        self.cache_manager = RedisSemanticCacheManager()
+        self.cache_manager2 = ExactMatchRedisCache() # exact match dont need llm_string
 
-    # Generation engine's query(original) with original query reranking (low score short processing time)(not using)
+    # Generation engine's query(original) with original query reranking
     def process_query_normal(self, query: str, llm_string: str):
         """
         Process an LLM query through retrieval, re-ranking, and caching.
@@ -53,7 +55,7 @@ class RAGSystem:
             return augmented_query_data
         except Exception as e:
             print(f"\nAn error occurred during testing: {e}")
-    # GPT4 generated Multi query with generated query reranking (best but long processing time)
+    # GPT4 generated Multi query with generated query reranking
     def process_query_v2(self, query: str):
         """
         Process an LLM query through Multi query retrieval, Multi query re-ranking, and caching.
@@ -62,7 +64,7 @@ class RAGSystem:
         """
         try:
             # Step 1: Check cache
-            cached_document = self.cache_manager.lookup(query)
+            cached_document = self.cache_manager2.lookup(query)
             if cached_document:
                 print(f"Cache hit! Cached document:\n{cached_document}")
                 return cached_document
@@ -95,12 +97,12 @@ class RAGSystem:
             augmented_query_data = self.augmenter.augment_query_with_document(query, reranked_documents)
             
             # Step 5: Update cache
-            self.cache_manager.update(query, augmented_query_data)
+            self.cache_manager2.update(query, augmented_query_data)
 
             return augmented_query_data
         except Exception as e:
             print(f"\nAn error occurred during testing: {e}")
-    # GPT4 generated Multi query with original query reranking (low score short processing time)
+    # GPT4 generated Multi query with original query reranking
     def process_query_mix(self, query: str):
             """
             Process an LLM query through Multi query retrieval, re-ranking using original query, and caching.
@@ -109,7 +111,7 @@ class RAGSystem:
             """
             try:
                 # Step 1: Check cache
-                cached_document = self.cache_manager.lookup(query)
+                cached_document = self.cache_manager2.lookup(query)
                 if cached_document:
                     print(f"Cache hit! Cached document:\n{cached_document}")
                     return cached_document
@@ -142,11 +144,58 @@ class RAGSystem:
                 augmented_query_data = self.augmenter.augment_query_with_document(query, reranked_documents)
                 
                 # Step 5: Update cache
-                self.cache_manager.update(query, augmented_query_data)
+                self.cache_manager2.update(query, augmented_query_data)
 
                 return augmented_query_data
             except Exception as e:
                 print(f"\nAn error occurred during testing: {e}")
+    # GPT4 generated Multi query with BM25 reranking 
+    def process_query_v3(self, query: str):
+        """
+        Process an LLM query through Multi query retrieval, Multi query re-ranking, and caching.
+        :param query: The input query.
+        :return: Augmented query (with context).
+        """
+        try:
+            # Step 1: Check cache
+            cached_document = self.cache_manager2.lookup(query)
+            if cached_document:
+                print(f"Cache hit! Cached document:\n{cached_document}")
+                return cached_document
+
+            # Step 2: Retrieve documents and generated queries using the retriever
+            print("\n--- Retrieving Documents ---")
+            retrieved_docs, generated_queries = self.retriever.retrieve_multi_query(query)
+            if not retrieved_docs:
+                print("No documents retrieved. Cannot proceed with re-ranking.")
+                return
+
+            # Display the retrieved documents
+            print(f"\nRetrieved {len(retrieved_docs)} documents:")
+            print(self.retriever.format_results(retrieved_docs))
+
+            # Display the generated queries
+            print("\nGenerated Queries:")
+            for GEN_query in generated_queries:
+                print(GEN_query)
+
+            # Step 3: Re-rank the retrieved documents across all queries
+            print("\n--- Re-ranking Documents Across Queries ---")
+            reranked_documents = self.re_ranker2.rerank_documents(query, retrieved_docs)
+
+            # Display the re-ranked results
+            print("\nRe-ranked Documents Across Queries:")
+            print(self.re_ranker2.format_results(reranked_documents))
+
+            # Step 4: Augment query with top document
+            augmented_query_data = self.augmenter.augment_query_with_document2(query, reranked_documents)
+            
+            # Step 5: Update cache
+            self.cache_manager2.update(query, augmented_query_data)
+
+            return augmented_query_data
+        except Exception as e:
+            print(f"\nAn error occurred during testing: {e}")
 
 #=== Testing ===
 def test_normal():
@@ -197,6 +246,24 @@ def test_mix():
 
     # Process the query through the RAG pipeline
     result = rag.process_query_mix(query)
+
+    print(result)
+    #rag.cache_manager.clear_cache(llm_string)
+
+def test_v3():
+    # Initialize RAG pipeline
+    rag = RAGSystem()
+
+    # Sample user query
+    #query = "Age: 78, Gender: female, Medications: Digoxin (OD 0.125mg), Fluticasone (BID 2 puff), Warfarin (OD 5), Conditions: Essential hypertension (BA00), Iron deficiency anaemia (3A00), Mixed hyperlipidaemia (5C80.2)"
+
+    query = "Age: 92, Gender: male, Medication: Metoprolol (OD 100mg), Warfarin (OD 5mg), Amiodarone (OD 800mg), Simvastatin (OD 10mg), Condition: Obesity in adults, Iron deficiency anaemia, Mixed hyperlipidaemia, Osteoporosis, Congestive heart failure, Acute myocardial infarction, Intermediate hyperglycaemia"
+    
+    # For caching (llm_string is a string representation of the LLM configuration)
+    #llm_string = "gpt-4"
+
+    # Process the query through the RAG pipeline
+    result = rag.process_query_v3(query)
 
     print(result)
     #rag.cache_manager.clear_cache(llm_string)
